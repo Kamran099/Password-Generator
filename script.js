64c5101ac79aa934c482ddfdd9619a0c7122ec25
Kamran <kamran-anwar99@hotmail.com>
2022-07-16 22:28:49 +0100

    JS functions and Styles

---
+++ script.js
@@ -0,0 +1,32 @@
+const characterAmountRange = document.getElementById('characterAmountRange')
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers');
+const includeSymbolsElement = document.getElementById('includeSymbols');
+
+characterAmountNumber.addEventListener('input', syncCharacterAmount);
+characterAmountRange.addEventListener('input', syncCharacterAmount);
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const characterAmount = characterAmountNumber.value;
+    const includeUppercase = includeUppercaseElement.checked;
+    const includeNumbers = includeNumbersElement.checked;
+    const includeSymbols = includeSymbolsElement.checked;
+
+    const password = generatePassword(characterAmount, includeUppercase, includNumbers, includeSymbols)
+});
+
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+
+}
+
+function syncCharacterAmount(e) {
+    const value = e.target.value;
+    characterAmountNumber.value = value;
+    characterAmountRange.value = value;
+}
+
+
+
+
